0acbdeace8db28e83c5a5a9fca8d133513c1e158
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-10-11 21:21:05 +0800

    Update install-erlang-from-source-code.sh

--- install-erlang-from-source-code.sh
+++ install-erlang-from-source-code.sh
@@ -22,6 +22,7 @@ pkg-config --version
 # compile the source code
 wget https://github.com/erlang/otp/releases/download/OTP-26.2.5.4/otp_src_26.2.5.4.tar.gz
 tar -xzvf otp_src_26.2.5.4.tar.gz
+cd otp_src_26.2.5.4
 ./configure
 make
 make install
