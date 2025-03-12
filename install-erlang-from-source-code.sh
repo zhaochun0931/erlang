d38e0f312f42cc29c85555acd1e3077f9cac61a3
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-03-12 08:32:22 +0800

    Update install-erlang-from-source-code.sh

--- install-erlang-from-source-code.sh
+++ install-erlang-from-source-code.sh
@@ -1,10 +1,3 @@
-# install erlang from the source code
-
-
-otp_src_26.2.5.4.tar.gz
-
-
-
 # prerequsite
 
 sudo apt update
@@ -19,13 +12,18 @@ pkg-config --version
 
 
 
-# compile the source code
+# download and compile the source code
 
-wget https://github.com/erlang/otp/releases/download/OTP-26.2.5.4/otp_src_26.2.5.4.tar.gz
-tar -xzvf otp_src_26.2.5.4.tar.gz
-cd otp_src_26.2.5.4
+wget https://github.com/erlang/otp/releases/download/OTP-27.3/otp_src_27.3.tar.gz
+
+tar -xzvf otp_src_27.3.tar.gz
+
+cd otp_src_27.3/
+
 ./configure --with-ssl
+
 make
+
 make install
 
 
@@ -33,27 +31,11 @@ make install
 
 
 
-apt install -y build-essential libncurses5-dev libncursesw5-dev
 
 
 
 
-wget https://github.com/erlang/otp/releases/download/OTP-27.3/otp_src_27.3.tar.gz
-
-
-tar -xvf otp_src_27.3.tar.gz
-
-
-cd otp_src_27.3/
-
-
-./configure --with-ssl && make && make install
-
-
-
-
-
-
+# vertify erlang version
 
 erl -eval '{ok, Version} = file:read_file(filename:join([code:root_dir(), "releases", erlang:system_info(otp_release), "OTP_VERSION"])), io:fwrite(Version), halt().' -noshell
 
