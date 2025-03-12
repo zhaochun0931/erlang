9cd51a0a452c40ec681d7744675c144fa30e615c
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-03-12 11:01:03 +0800

    Update install-erlang-from-source-code.sh

--- install-erlang-from-source-code.sh
+++ install-erlang-from-source-code.sh
@@ -33,15 +33,3 @@ make install
 
 
 
-
-
-# vertify erlang version
-
-erl -eval '{ok, Version} = file:read_file(filename:join([code:root_dir(), "releases", erlang:system_info(otp_release), "OTP_VERSION"])), io:fwrite(Version), halt().' -noshell
-
-
-
-
-
-
-
